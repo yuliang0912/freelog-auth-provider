926c23439365450b1ad66f31acc01e7a08b6690a
yuliang <yu.liang@freelog.com>
2018-03-06 16:52:14 +0800

    1.修改测试环境配置

--- config/config.test.js
+++ config/config.test.js
@@ -4,13 +4,33 @@
 
 'use strict'
 
-module.exports = appInfo => {
-    return {
+module.exports = {
 
-        gatewayUrl: "http://192.168.164.129",
+    gatewayUrl: "http://172.18.215.224:8895",
 
-        mongo: {
-            uri: "mongodb://192.168.0.99:27017/auth"
+    mongoose: {
+        url: "mongodb://root:Ff233109@dds-wz9b5420c30a27941546-pub.mongodb.rds.aliyuncs.com:3717,dds-wz9b5420c30a27942267-pub.mongodb.rds.aliyuncs.com:3717/auth?replicaSet=mgset-5016983"
+    },
+
+    knex: {
+        contract: {
+            connection: {
+                host: 'rm-wz9wj9435a0428942.mysql.rds.aliyuncs.com',
+                user: 'freelog',
+                password: 'Ff@233109',
+                database: 'fr_contract',
+            },
+            debug: false
+        },
+    },
+
+    rabbitMq: {
+        connOptions: {
+            host: '172.18.215.224',
+            port: 5672,
+            login: 'guest',
+            password: 'guest',
+            authMechanism: 'AMQPLAIN'
         },
     }
 }
