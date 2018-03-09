6e47b2e9bb9e4d50d624b6f6b6af77e4b10888ba
yuliang <yu.liang@freelog.com>
2018-03-09 11:12:45 +0800

    Squashed commit of the following:
    commit f7a8d16f8dacda0c1abcb88000cccba8d7bfa1a7
    Author: yuliang <yu.liang@freelog.com>
    Date:   Fri Mar 9 10:42:22 2018 +0800
    
        1.修改测试环境配置
    
    commit 341096b4484c9ef6f8ef45731a3bcdbf225ef684
    Author: yuliang <yu.liang@freelog.com>
    Date:   Thu Mar 8 15:29:58 2018 +0800
    
        1.修改配置
    
    commit 7588c1bdd7d2a31ec0ddb08b6867364bf269ba8c
    Author: yuliang <yu.liang@freelog.com>
    Date:   Thu Mar 8 15:12:54 2018 +0800
    
        1.修改配置
    
    commit 4d9cc0a38d64d7bbb01c8974236377227f89573b
    Author: yuliang <yu.liang@freelog.com>
    Date:   Thu Mar 8 15:09:06 2018 +0800
    
        1.修改配置
    
    commit 926c23439365450b1ad66f31acc01e7a08b6690a
    Author: yuliang <yu.liang@freelog.com>
    Date:   Tue Mar 6 16:52:14 2018 +0800
    
        1.修改测试环境配置
    
    commit 23cddebaf2b49d1dba3f1ac2405f536359ef3117
    Author: yuliang <yu.liang@freelog.com>
    Date:   Tue Mar 6 16:42:47 2018 +0800
    
        1.修改dockerfile文件

--- config/config.test.js
+++ config/config.test.js
@@ -4,13 +4,33 @@
 
 'use strict'
 
-module.exports = appInfo => {
-    return {
+module.exports = {
 
-        gatewayUrl: "http://192.168.164.129",
+    gatewayUrl: "http://172.18.215.224:8895/test",
 
-        mongo: {
-            uri: "mongodb://192.168.0.99:27017/auth"
+    mongoose: {
+        url: "mongodb://172.18.215.229:27017/auth"
+    },
+
+    knex: {
+        contract: {
+            connection: {
+                host: 'rm-wz93t7g809kthrub7.mysql.rds.aliyuncs.com',
+                user: 'freelog_test',
+                password: 'Ff@233109',
+                database: 'fr_contract',
+            },
+            debug: false
+        },
+    },
+
+    rabbitMq: {
+        connOptions: {
+            host: '172.18.215.229',
+            port: 5672,
+            login: 'test_user_auth',
+            password: 'test_user_2018',
+            authMechanism: 'AMQPLAIN'
         },
     }
 }
