3d4781402feb09a9622617db325b8cfbf80e1bb8
yuliang <yu.liang@freelog.com>
2018-12-27 10:51:57 +0800

    1.修改网关配置

--- config/config.test.js
+++ config/config.test.js
@@ -12,7 +12,7 @@ module.exports = {
         }
     },
 
-    gatewayUrl: "http://172.18.215.224:8895/test",
+    gatewayUrl: "http://172.18.215.229:6895/test",
 
     mongoose: {
         url: "mongodb://172.18.215.231:27018/auth"
