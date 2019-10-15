961bdfe67be5780fcadab00d83c103552473e0e3
yuliang <yu.liang@freelog.com>
2019-10-15 10:14:35 +0800

    1.修改返回值

--- app/controller/auth/presentable-v1.js
+++ app/controller/auth/presentable-v1.js
@@ -30,7 +30,7 @@ module.exports = class PresentableOrResourceAuthController extends Controller {
 
         const authResult = await ctx.service.presentableAuthService.presentableAllChainAuth(presentableInfo)
         if (!authResult.isAuth) {
-            authResult.data.presentableInfo = lodash.pick(presentableInfo, ["presentableId", "presentableName", "releaseInfo", "intro", "policies"])
+            authResult.data.presentableInfo = lodash.pick(presentableInfo, ["presentableId", "presentableName", "intro", "nodeId", "policies", "releaseInfo"])
         }
 
         await this._responseSubDependToHeader(presentableId)
